003433a677d0d247b8eba4a8566ff52b1e836748
Brendan O'Brien <bobrien@pursuit.org>
2024-07-11 14:11:02 -0400

    creates postgres promise for our db

---
+++ db/dbConfig.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise')();
+require('dotenv').config();
+
+// PG_PASSWORD does not exist locally 
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+};
+
+const db = pgp(cn);
+
+module.exports = db;
