7a589af8b237df7732249407e7152d4be98042d5
Brendan O'Brien <bobrien@pursuit.org>
2024-07-11 11:49:06 -0400

    sets up server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Colors App!');
+})
+
+
+module.exports = app;
