b384405537e9b38c17e22a0088667140e3edb76d
Brendan O'Brien <bobrien@pursuit.org>
2024-07-12 14:50:09 -0400

    creates /:id route

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -13,6 +13,16 @@ colors.get('/', async (req, res) => {
      } // end if/else
 }); // end get
 
+// Show: localhost:4001/colors/<id>
+colors.get('/:id}', async (req, res) => {
+    const { id } = req.params;
+    const color = await getColor(id);
+    if (color) {
+        res.status(200).json(color);
+    } else {
+        res.status(500).json({ error: "server error"});
+    } // end if/else
+}) // end get
 
 
 module.exports = colors;
