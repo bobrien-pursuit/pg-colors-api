d9236c5bdc25842a9190e8eefc5149b58e02b49d
Brendan O'Brien <bobrien@pursuit.org>
2024-07-15 12:45:24 -0400

    adds status messages

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -47,8 +47,12 @@ colors.put('/:id', checkName, checkBoolean, async (req, res) => {
     const { id } = req.params;
     const updatedColor = await updateColor(id, req.body);
     res.status(200).json(updatedColor);
+    if(updatedColor.id){
+        res.status(200).json(updatedColor);
+    } else {
+        res.status(404).json({ error: "Color not found."});
+    }
 })
-
 
 
 module.exports = colors;
