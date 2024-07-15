9d9c4d8bc998f629db7a017d0879086d9524e769
Brendan O'Brien <bobrien@pursuit.org>
2024-07-15 12:49:31 -0400

    removed double response

--- controllers/colorsController.js
+++ controllers/colorsController.js
@@ -46,7 +46,6 @@ colors.delete('/:id', async (req, res) => {
 colors.put('/:id', checkName, checkBoolean, async (req, res) => {
     const { id } = req.params;
     const updatedColor = await updateColor(id, req.body);
-    res.status(200).json(updatedColor);
     if(updatedColor.id){
         res.status(200).json(updatedColor);
     } else {
